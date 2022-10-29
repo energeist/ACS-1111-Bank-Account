ef65a259635d81550947b3337580bf24cec3ebf2
Mark Rattle <mark.rattle@gmail.com>
2022-10-29 16:24:29 -0400

    required example code

--- bank-account.py
+++ bank-account.py
@@ -3,7 +3,6 @@ ACS-1111 Bank Account assignment
 """
 
 class BankAccount: 
-
     def __init__(self, full_name, account_number):
         self.full_name = full_name
         self.account_number = account_number
@@ -18,19 +17,24 @@ class BankAccount:
     def get_balance(self):
         print(f"Hi {self.full_name}, balance is: {self.balance:.2f}")
     
-    def add_interest(monthly_interest_rate = 0.00083):
-        interest = balance * monthly_interest_rate
-        balance += interest
+    def add_interest(self, monthly_interest_rate = 0.00083):
+        interest = self.balance * monthly_interest_rate
+        self.balance += interest
 
     def print_statement(self):
         print(f"{self.full_name}")
         print(f"Account No.: ****{self.account_number[4:len(self.account_number)]}")
-        print(f"Balance: {self.balance}")
+        print(f"Balance: {self.balance:.2f}")
+        print()
 
 # mark_account = BankAccount(full_name, account_number)
 # homer_account = BankAccount(full_name, account_number)
 mitchell_account = BankAccount("Mitchell Mitcherson", "03141592")
 
+mitchell_account.deposit(400000)
 mitchell_account.print_statement()
-
+mitchell_account.add_interest()
+mitchell_account.print_statement()
+mitchell_account.withdraw(150)
+mitchell_account.print_statement()
 
